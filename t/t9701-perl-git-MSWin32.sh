ColMelvin/git

Perl: Support spaces in command arguments on MSWin32
On a native Windows build of Perl (where $^O equals 'MSWin32'), like
Strawberry or ActiveState Perl, the Git.pm module follows a unique code
path, one that alters the behavior of piped Git commands.  This special
path failed to handle spaces contained in an argument to Git.  Remove
that limitation by improving Windows command line support.

On Windows, the command line is part of the operating system; it is not
provided by a shell.  This system requires certain characters (\, ") to
be escaped (in certain cases).  The cmd.exe command prompt rests on top
of the command line and it has its own, additional escaping mechanism.
When creating pipes, an MSWin32 build of Perl will either use the system
call, CreateProcess (which takes the command line), or, if interpreted
characters are detected, the command prompt, cmd.exe.

Add a function to unescape command line arguments, one that will work
with CreateProcess or with cmd.exe, as appropriate.  This ensures any
Git command will have its arguments parsed correctly, regardless of
content.

Additionally, include tests to verify that Perl's system() behavior
remains unchanged -- notably, that the list of interpreted characters
(for selecting cmd.exe) does not change.  These tests need an MSWin32
build of Perl in the PATH to work; otherwise, they are skipped.

This change will improve commands (or batch scripts) run in CMD, where
native (MSWin32) builds of Perl are all that is available in the PATH
(unless the user opts to install Unix utils in the PATH, which is not
recommended in the Git for Windows installer).  Commands run in MSYS
Perl, which is included with Git for Windows, will not change (as it
never had a problem).

Closes: #1602
Signed-off-by: Chris Lindee <chris.lindee+github@gmail.com>

t/t9701-perl-git-MSWin32.sh
@@ -1,0 +1,45 @@
+#!/bin/sh
+#
+# Copyright (c) 2018 Chris Lindee
+#
+
+test_description='perl interface (Git.pm) on MSWin32'
+. ./test-lib.sh
+
+find_MSWin32_perl() {
+	local perl
+	for perl in $(which -a perl); do
+		if "$perl" -e 'exit 1 if $^O ne q{MSWin32}'; then
+			echo "$perl"
+			return 0
+		fi
+	done
+	return 1
+}
+
+MSWin32_Perl="$(find_MSWin32_perl)"
+if [ $? -ne 0 ]; then
+	skip_all='skipping perl on MSWin32 interface tests, MSWin32 perl not available'
+	test_done
+fi
+
+"$MSWin32_Perl" -MTest::More -e 0 2>/dev/null || {
+	skip_all="MSWin32 Perl Test::More unavailable, skipping test"
+	test_done
+}
+
+# The external test will outputs its own plan
+test_external_has_tap=1
+
+# Ensure paths are recognized by Windows executables.
+perl_test_path="$TEST_DIRECTORY"/t9701/test.pl
+if test_have_prereq CYGWIN || test_have_prereq MINGW; then
+	perl_test_path="$(cygpath -w "$perl_test_path")"
+	GITPERLLIB="$(cygpath -w -p "$GITPERLLIB")"
+fi
+
+test_external_without_stderr \
+	'Windows command line (Perl API)' \
+	"$MSWin32_Perl" "$perl_test_path"
+
+test_done

nosnjinoR/Windows

t/t9701-perl-git-MSWin32.sh
@@ -1,0 +1,45 @@
+#!/bin/sh
+#
+# Copyright (c) 2018 Chris Lindee
+#
+
+test_description='perl interface (Git.pm) on MSWin32'
+. ./test-lib.sh
+
+find_MSWin32_perl() {
+	local perl
+	for perl in $(type -ap perl); do
+		if "$perl" -e 'exit 1 if $^O ne q{MSWin32}'; then
+			echo "$perl"
+			return 0
+		fi
+	done
+	return 1
+}
+
+MSWin32_Perl="$(find_MSWin32_perl)"
+if [ $? -ne 0 ]; then
+	skip_all='skipping perl on MSWin32 interface tests, MSWin32 perl not available'
+	test_done
+fi
+
+"$MSWin32_Perl" -MTest::More -e 0 2>/dev/null || {
+	skip_all="MSWin32 Perl Test::More unavailable, skipping test"
+	test_done
+}
+
+# The external test will outputs its own plan
+test_external_has_tap=1
+
+# Ensure paths are recognized by Windows executables.
+perl_test_path="$TEST_DIRECTORY"/t9701/test.pl
+if test_have_prereq CYGWIN || test_have_prereq MINGW; then
+	perl_test_path="$(cygpath -w "$perl_test_path")"
+	GITPERLLIB="$(cygpath -w -p "$GITPERLLIB")"
+fi
+
+test_external_without_stderr \
+	'Windows command line (Perl API)' \
+	"$MSWin32_Perl" "$perl_test_path"
+
+test_done