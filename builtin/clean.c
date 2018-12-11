e004e1b0eb6cda0ceb9e8d6e9680081f3306fa2f
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-12-11 12:17:49 +0100

    clean: make use of FSCache
    The `git clean` command needs to enumerate plenty of files and
    directories, and can therefore benefit from the FSCache.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/clean.c
+++ builtin/clean.c
@@ -983,6 +983,7 @@ int cmd_clean(int argc, const char **argv, const char *prefix)
 
 	if (read_cache() < 0)
 		die(_("index file corrupt"));
+	enable_fscache(active_nr);
 
 	if (!ignored)
 		setup_standard_excludes(&dir);
@@ -1072,6 +1073,7 @@ int cmd_clean(int argc, const char **argv, const char *prefix)
 		strbuf_reset(&abs_path);
 	}
 
+	disable_fscache();
 	strbuf_release(&abs_path);
 	strbuf_release(&buf);
 	string_list_clear(&del_list, 0);
