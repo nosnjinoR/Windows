96ab0057f0f829291b304dc28da7e0c4af49259c
Philip Oakley <philipoakley@iee.org>
2019-03-11 17:04:51 +0000

    zlib.c: deflateBound size coercion note
    The 'size' is 32 bits on Windows, but 64 bits on Linux and needs careful
    selection a-priori of NO_DEFLATE_BOUND
    
    Signed-off-by: Philip Oakley <philipoakley@iee.org>

--- packfile.h
+++ packfile.h
@@ -146,19 +146,11 @@ off_t nth_packed_object_offset(const struct packed_git *, uint32_t n);
  */
 off_t find_pack_entry_one(const unsigned char *sha1, struct packed_git *);
 
-<<<<<<< ours
 int is_pack_valid(struct packed_git *);
-void *unpack_entry(struct repository *r, struct packed_git *, off_t, enum object_type *, unsigned long *);
-unsigned long unpack_object_header_buffer(const unsigned char *buf, unsigned long len, enum object_type *type, unsigned long *sizep);
-unsigned long get_size_from_delta(struct packed_git *, struct pack_window **, off_t);
-int unpack_object_header(struct packed_git *, struct pack_window **, off_t *, unsigned long *);
-=======
-extern int is_pack_valid(struct packed_git *);
-extern void *unpack_entry(struct repository *r, struct packed_git *, off_t, enum object_type *, size_t *);
-extern size_t unpack_object_header_buffer(const unsigned char *buf, size_t len, enum object_type *type, size_t *sizep);
-extern size_t get_size_from_delta(struct packed_git *, struct pack_window **, off_t);
-extern int unpack_object_header(struct packed_git *, struct pack_window **, off_t *, size_t *);
->>>>>>> theirs
+void *unpack_entry(struct repository *r, struct packed_git *, off_t, enum object_type *, size_t *);
+size_t unpack_object_header_buffer(const unsigned char *buf, unsigned long len, enum object_type *type, size_t *sizep);
+size_tg get_size_from_delta(struct packed_git *, struct pack_window **, off_t);
+int unpack_object_header(struct packed_git *, struct pack_window **, off_t *, size_t *);
 
 void release_pack_memory(size_t);
 
