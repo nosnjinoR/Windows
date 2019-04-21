e8271887a0b2e93c57f423212ad3a45a515efa8b
Philip Oakley <philipoakley@iee.org>
2019-04-21 16:54:25 +0100

    squash! zlib.c: deflateBound size coercion note
    (also updated packfile.h)
    
    Signed-off-by: Philip Oakley <philipoakley@iee.org>

--- packfile.h
+++ packfile.h
@@ -148,8 +148,8 @@ off_t find_pack_entry_one(const unsigned char *sha1, struct packed_git *);
 
 int is_pack_valid(struct packed_git *);
 void *unpack_entry(struct repository *r, struct packed_git *, off_t, enum object_type *, size_t *);
-size_t unpack_object_header_buffer(const unsigned char *buf, unsigned long len, enum object_type *type, size_t *sizep);
-size_tg get_size_from_delta(struct packed_git *, struct pack_window **, off_t);
+size_t unpack_object_header_buffer(const unsigned char *buf, size_t len, enum object_type *type, size_t *sizep);
+size_t get_size_from_delta(struct packed_git *, struct pack_window **, off_t);
 int unpack_object_header(struct packed_git *, struct pack_window **, off_t *, size_t *);
 
 void release_pack_memory(size_t);
