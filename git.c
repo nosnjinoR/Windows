zhangzhen321/git

Pass PID of git process to hooks.

git.c
@@ -338,6 +338,7 @@ static int run_pre_command_hook(const char **argv)
 
 	/* call the hook proc */
 	argv_array_pushv(&sargv, argv);
+	argv_array_pushf(&sargv, "--git-pid=%"PRIuMAX, (uintmax_t)getpid());
 	ret = run_hook_argv(NULL, "pre-command", sargv.argv);
 
 	if (!ret)

nosnjinoR/Windows

Signed-off-by: Alejandro Pauly <alpauly@microsoft.com>

git.c
@@ -452,6 +452,7 @@ static int run_pre_command_hook(const char **argv)
 
 	/* call the hook proc */
 	strvec_pushv(&sargv, argv);
+	strvec_pushf(&sargv, "--git-pid=%"PRIuMAX, (uintmax_t)getpid());
 	strvec_pushv(&opt.args, sargv.v);
 	ret = run_hooks_opt("pre-command", &opt);
 